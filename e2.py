d8e0738a59d7b64241bb6a5f2bafc11a7253a69a
franco.chavez.7e7 <franco.chavez.7e7@itb.cat>
2024-01-23 12:05:00 +0100

    23-01 franco ex 2

--- e2.py
+++ e2.py
@@ -2,5 +2,14 @@
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 23/01/2023
 ASIXc1C M03 UF1
-Descripcion:
+Descripcion:Programa que generi una llista de 100 nombres aleatoris entre 1 i 50. Obtenir la mitja dels nombres que es troben a les posicions parelles i la mitja del nombre de les posicions senars.
+Per aconseguir nombres aleatoris en Python podem utilitzar la funció random.randint(limitInferior,limitSuperior)
 """
+import random
+
+suma_pares = 0
+suma_impares = 0
+tupla = []
+
+for x in range (1,51):
+    tupla.append(random.randint(1,50))
