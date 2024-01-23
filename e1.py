72a9d1a21991422be07c4b02d2b23daf3ba589b6
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-01-23 10:06:33 +0100

    Update

--- e1.py
+++ e1.py
@@ -2,5 +2,7 @@
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 23/01/2023
 ASIXc1C M03 UF1
-Descripcion:
+Descripcion:Fer un programa de càlcul de temperatures del mar. Les tasques a fer són:
+Calcular per a l’any  2022: temperatura màxima, mínima i mitjana
+Calcular per període 2000 a 2022: temperatura màxima, mínima i mitjana
 """
