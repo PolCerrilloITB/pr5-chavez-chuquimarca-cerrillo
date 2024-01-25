0e082ceb27ba98e35642ad4f419580fe0fa6b568
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-01-25 07:54:25 +0100

    Update

--- e3.py
+++ e3.py
@@ -2,5 +2,8 @@
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 23/01/2023
 ASIXc1C M03 UF1
-Descripcion:
+Descripcion: Programa de traducció d’insults. Crear una estructura de dues dimensions amb els insults
+en català i afegir la traducció en castellà, anglès i klingon
+El programa, demanarà a l’usuari que escrigui per teclat un insult, en català,
+i el mostrarà traduït a castellà, anglès i klingon.
 """
